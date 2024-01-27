4da2469d1eaea22282b2b49984c18971f849198f
Muntasir Meah <mmeah@bu.edu>
2024-01-27 12:39:21 -0500

    cs 506 exercise part b

--- polynomial/polynomial.py
+++ polynomial/polynomial.py
@@ -37,12 +37,6 @@ class Mul:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
 
@@ -52,12 +46,8 @@ class Div:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+
+print(poly)
